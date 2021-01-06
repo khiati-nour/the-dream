ef051a739bbb8f44be9606c74cf08c48197d3c77
Khiati Houda <khiatihouda@gmail.com>
2021-01-06 11:06:21 +0100

    add the converter

--- index.php
+++ index.php
@@ -6,14 +6,35 @@
 </head>
 <body>
 <h2>HTML Forms</h2>
+<?php
+if (isset($_GET['submit'])){
 
-<form action="action_page.php" method = "post">
+    $Euro = $_GET['euro'];
+    $result = ($Euro * 162.35)." "."DZ";
+
+
+
+}
+if (isset($_GET['change'])){
+
+    $Dinar = $_GET['dinar'];
+    $result2 = ($Dinar / 162.35)." "."EURO";
+
+
+
+}
+?>
+<form >
     <label for="Euro">Euro</label><br>
     <input type="text"  name="euro" ><br>
-    <label for="Dinnar">Dinnar (DZ)</label><br>
-    <input type="text" name="dinnar" ><br><br>
-    <input type="submit" value="Submit">
+    <button type="change" value="change" name="change">change</button><br>
+    <label for="Dinar">Dinar (DZ)</label><br>
+    <input type="text" name="dinar" value="<?php echo @$result;?>"/><br><br>
+    <button type="submit" value="Submit" name="submit">Calculate</button>
 </form>
+
+
+
 
 </body>
 </html>

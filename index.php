55a8920248bdac3079df12d6fe22d813d5e85fc0
Khiati Houda <khiatihouda@gmail.com>
2021-01-06 14:01:37 +0100

    add float to the input value

--- index.php
+++ index.php
@@ -10,23 +10,19 @@
 if (isset($_GET['submit'])){
 
     $Euro = $_GET['euro'];
-    $result = ($Euro * 162.35)." "."DZ";
+    $result = ((float)$Euro * 162.35)." "."DZ";
 
-
-
-}
-if (isset($_GET['change'])){
 
     $Dinar = $_GET['dinar'];
-    $result2 = ($Dinar / 162.35)." "."EURO";
-
+    $result2 = ((float)$Dinar / 162.35)." "."EURO";
 
 
 }
+
 ?>
 <form >
     <label for="Euro">Euro</label><br>
-    <input type="text"  name="euro" value="<?php echo @$result2;?>"<br>
+    <input type="text"  name="euro" value="<?php echo @$result2; ?>"<br>
     <button type="change" value="change" name="change">change</button><br>
     <label for="Dinar">Dinar (DZ)</label><br>
     <input type="text" name="dinar" value="<?php echo @$result;?>"/><br><br>

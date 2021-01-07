8d191b5584b9ae719f3a6cb4f4e958833577a922
Khiati Houda <khiatihouda@gmail.com>
2021-01-07 10:03:34 +0100

    finish the exo with some issues

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta charset="UTF-8">
     <title>Title</title>
 </head>
@@ -8,29 +9,24 @@
 <h2>HTML Forms</h2>
 <?php
 if (isset($_GET['submit'])){
-
-    $Euro = $_GET['euro'];
-    $result = ((float)$Euro * 162.35)." "."DZ";
-
-
-    $Dinar = $_GET['dinar'];
-    $result2 = ((float)$Dinar / 162.35)." "."EURO";
+        $Dinar = $_GET['dinar'];
+        $Euro = $_GET['euro'];
+      echo  $result = ((float)$Euro * 162.35) . " " . "DZ";
+       echo $result2 = ((float)$Dinar / 162.35)." "."EURO";
 
 
 }
 
 ?>
 <form >
-    <label for="Euro">Euro</label><br>
-    <input type="text"  name="euro" value="<?php echo @$result2; ?>"<br>
-    <button type="change" value="change" name="change">change</button><br>
-    <label for="Dinar">Dinar (DZ)</label><br>
-    <input type="text" name="dinar" value="<?php echo @$result;?>"/><br><br>
-    <button type="submit" value="Submit" name="submit">Calculate</button>
+
+    <input type="text"  name="euro" value="<?php echo $result2; ?>""<br>
+    <button type="submit" value="Submit" name="submit" style = "border: none; color : white;"><i class="fa fa-exchange" style="font-size:15px;color:red;"></i> </button>
+    <input type="text" name="dinar" value="<?php echo $result; ?>"/><br><br>
+
+
+
 </form>
-
-
-
 
 </body>
 </html>
